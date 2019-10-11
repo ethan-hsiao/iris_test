d2fc3e4f201ae5c342c8b8df570ffbe577a4bbf0
Ethan Hsiao <ethan@Ethans-MacBook-Pro.local>
2019-10-11 16:11:01 -0700

    added selection of best model

--- iris_classifier.py
+++ iris_classifier.py
@@ -1,5 +1,4 @@
 
-# Load libraries
 import pandas
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
@@ -15,18 +14,17 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
 
-# Load dataset
-url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+url = "iris.csv"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pandas.read_csv(url, names=names)
 
 # shape
-print(dataset.shape)
+# print(dataset.shape)
 
 
 # box and whisker plots
-dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-plt.show()
+# dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+# plt.show()
 
 # Split-out validation dataset
 array = dataset.values
@@ -40,4 +38,30 @@ X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(
 seed=7
 scoring = 'accuracy'
 
+# Spot check algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# Evaluate each model in sequence
+results = []
+names = []
+for name, model in models:
+	kfold = model_selection.KFold(n_splits=10, random_state=seed)
+	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+	results.append(cv_results)
+	names.append(name)
+	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+	print(msg)
+
+# To compare algorithm accuracy
+fig = plt.figure()
+fig.suptitle('Algorithm Comparison')
+ax = fig.add_subplot(111)
+plt.boxplot(results)
+ax.set_xticklabels(names)
+plt.show()
 
